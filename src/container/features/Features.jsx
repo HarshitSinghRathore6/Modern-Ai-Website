baf6484506d56d931f0e1cef449c0f110ff7f01b
HarshitSinghRathore6 <155346714+HarshitSinghRathore6@users.noreply.github.com>
2024-10-09 15:43:34 +0530

    .

--- src/container/features/Features.jsx
+++ src/container/features/Features.jsx
@@ -3,15 +3,7 @@ import "./features.css";
 
 const Features = ({ title, text }) => {
   return (
-    <div className="gpt3__features-container__feature">
-      <div className="gpt3__features-container__feature-title">
-        <div />
-        <h1>{title}</h1>
-      </div>
-      <div className="gpt3__features-container_feature-text">
-        <p>{text}</p>
-      </div>
-    </div>
+    <div></div>
   );
 };
 
